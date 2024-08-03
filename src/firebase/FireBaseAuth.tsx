14f45e8377e4c49c5691359d2df3bfe188d816ce
Anurag Shrestha <ashrestha@unm.edu>
2024-08-02 21:59:03 -0600

    create firebase config

---
+++ src/firebase/FireBaseAuth.tsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCjcqwJtA7P2tkbkGj16Og322QOMAIhIJw",
+  authDomain: "lendven-37dcb.firebaseapp.com",
+  databaseURL: "https://lendven-37dcb-default-rtdb.firebaseio.com",
+  projectId: "lendven-37dcb",
+  storageBucket: "lendven-37dcb.appspot.com",
+  messagingSenderId: "308332149137",
+  appId: "1:308332149137:web:a795dc83963f7343100230",
+  measurementId: "G-8RT03C1MZ2"
+};
+
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {persistence: getReactNativePersistence(ReactNativeAsyncStorage)});
