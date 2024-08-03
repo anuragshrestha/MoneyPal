9d33c311c16ed22b31d7bc55664a747952941d22
Anurag Shrestha <ashrestha@unm.edu>
2024-08-02 22:45:55 -0600

    created the firestore database

--- src/firebase/FireBaseAuth.tsx
+++ src/firebase/FireBaseAuth.tsx
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -15,3 +16,4 @@ const firebaseConfig = {
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {persistence: getReactNativePersistence(ReactNativeAsyncStorage)});
+export const FIREBASE_DB = getFirestore(FIREBASE_APP);
