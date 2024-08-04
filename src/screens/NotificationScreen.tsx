d07c1f0ee7372ea172b1df4580a5b2c9856dc859
Anurag Shrestha <ashrestha@unm.edu>
2024-08-03 18:49:22 -0600

    added a dummy notification screen

--- src/screens/NotificationScreen.tsx
+++ src/screens/NotificationScreen.tsx
@@ -1,12 +1,145 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Text, View, StyleSheet, SafeAreaView, ScrollView } from "react-native";
+import ScreenWrapper from "../components/ScreenWrapper";
+import { useColors } from "../contexts/ColorContext";
+import { formatNumber } from "../utils/utilFunction";
+import { GlobalStyles } from "../constants/globalstyles";
 
-const NotificationScreen = () => {
+
+export const info = {
+  friendList: [
+    {
+      name: "James",
+      amount_lent: 1450,
+      interest_earned: 50.12,
+      owes: 1500.12,
+      day: "Sunday",
+      time: "10:35 pm",
+    },
+    {
+      name: "David",
+      amount_lent: 200,
+      interest_earned: 14.91,
+      owes: 214.91,
+      day: "Monday",
+      time: "8:30 pm",
+    },
+    {
+      name: "Robert",
+      amount_lent: -400,
+      interest_earned: -20.01,
+      owes: -420.01,
+      day: "Sunday",
+      time: "6:10 Am",
+    },
+  ],
+};
+
+function NotificationScreen() {
+  const { colors } = useColors();
+
+  const interestTransactions = info.friendList.filter(
+    (item) => item.interest_earned !== 0
+  );
+  const amount_lentTransactions = info.friendList.filter(
+    (item) => item.amount_lent !== 0
+  );
+
   return (
-    <View>
-      <Text>NotificationScreen</Text>
-    </View>
-  )
+    <ScreenWrapper>
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <Text style={[styles.title, {color: colors.primary_black}]}>Recent Activity</Text>
+        {interestTransactions.map((item, index) => {
+          let amount = item.amount_lent > 0;
+          let positiveInterest = item.interest_earned >= 0;
+          let statusColor = positiveInterest ? "#00bb00" : "red";
+          return (
+            <View key={index} style={styles.outerInterest}>
+              <View style={styles.innerInterest}>
+                <Text style={{color: colors.primary_black, fontSize: 16}}>
+                  An automatic interest of{" "}
+                  <Text style={{ color: statusColor }}>
+                    ${formatNumber(Math.abs(item.interest_earned), 2)}
+                  </Text>{" "}
+                  was charged for {item.name}.
+                </Text>
+                <Text
+                  style={{
+                    color: colors.primary_darkergrey,
+                    paddingVertical: 5,
+                  }}
+                >
+                  {item.day} at {item.time}
+                </Text>
+              </View>
+            </View>
+          );
+        })}
+
+        {/* Displaying the amount lent or borrowed transactions only */}
+        {amount_lentTransactions.map((item, index) => {
+          let amount = item.amount_lent >= 0;
+          let borrowed = amount ? "lent by" : "borrowed from";
+          let positiveInterest = item.interest_earned >= 0;
+          let statusColor = positiveInterest ? "#00bb00" : "red";
+          let owesText = amount
+            ? `${item.name} owes you`
+            : `You owe ${item.name}`;
+
+          return (
+            <View key={index} style={styles.outerInterest}>
+              <View style={styles.innerInterest}>
+                <Text style={{color: colors.primary_black, fontSize: 16}}>
+                  A amount of{" "}
+                  <Text style={{ color: statusColor }}>
+                    ${amount ? item.amount_lent : -1 * item.amount_lent}
+                  </Text>{" "}
+                  was {borrowed} {item.name}.{" "}
+                </Text>
+                <Text
+                  style={{
+                    color: colors.primary_darkergrey,
+                    paddingVertical: 5,
+                  }}
+                >
+                  {" "}
+                  {owesText} a total of{" "}
+                  <Text style={{ color: statusColor }}>
+                    ${formatNumber(Math.abs(item.owes), 2)}
+                  </Text>
+                  .
+                </Text>
+              </View>
+            </View>
+          );
+        })}
+      </ScrollView>
+      </ScreenWrapper>
+  );
 }
 
-export default NotificationScreen
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollViewContent: {
+    padding: 10,
+  },
+  title: {
+    marginBottom: 15,
+    marginTop: 20,
+    fontSize: 22,
+    margin: 5,
+  },
+  outerInterest: {
+    borderBottomColor: GlobalStyles.colors.primary_black,
+    paddingVertical: 5,
+    borderBottomWidth: 1,
+    marginHorizontal: 10,
+  },
+  innerInterest: {
+    flexDirection: "column",
+    margin: 5,
+  },
+});
+
+export default NotificationScreen;
