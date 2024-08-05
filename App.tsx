aa589999eb91461045a15875632091080c23ba86
Anurag Shrestha <ashrestha@unm.edu>
2024-08-04 21:50:12 -0600

    wrapped the AppNavigator with userprovider

--- App.tsx
+++ App.tsx
@@ -3,13 +3,16 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import { ColorContextProvider, useColors } from "./src/contexts/ColorContext";
 import AppNavigator from "./src/screens/AppNavigator";
+import { UserProvider } from "./src/contexts/UserProvider";
 
 export default function App() {
-  const {theme} = useColors()
+  const { theme } = useColors();
   return (
     <View style={styles.container}>
       <ColorContextProvider>
-      <AppNavigator/>
+        <UserProvider>
+          <AppNavigator />
+        </UserProvider>
       </ColorContextProvider>
       <StatusBar style={theme !== "dark" ? "light" : "dark"} />
     </View>
