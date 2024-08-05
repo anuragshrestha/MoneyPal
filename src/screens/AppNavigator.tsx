46775c9551779dfb5e1e78925f6f3f34e31c81fa
Anurag Shrestha <ashrestha@unm.edu>
2024-08-04 21:57:59 -0600

    checking if the user is already authenticated then navogating to main screen else to the login screen

--- src/screens/AppNavigator.tsx
+++ src/screens/AppNavigator.tsx
@@ -1,17 +1,23 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LogInScreen from "./LogInScreen";
-import SignUpScreen from "./SignUpScreen";
 import BottomTab from "../navigation/BottomTab";
+import AuthStack from "../AuthStack/AuthStack";
+import { useUser } from "../contexts/UserProvider";
+import TransitionScreen from "./TransitionScreen";
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
+  const { user, isAuthenticating } = useUser();
+
+  if(isAuthenticating) return <TransitionScreen/>
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="LogIn" component={LogInScreen} />
-        <Stack.Screen name="SignUp" component={SignUpScreen} />
-        <Stack.Screen name="BottomTab" component={BottomTab} />
+        {user ? (
+          <Stack.Screen name="BottomTab" component={BottomTab} />
+        ) : (
+          <Stack.Screen name="AuthStack" component={AuthStack} />
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
