fe2a43f936cfd437e600e2266941e9a1ce00d053
Anurag Shrestha <ashrestha@unm.edu>
2024-08-04 20:46:51 -0600

    added a welcome text

--- src/components/Home/HomeScreenHeader.tsx
+++ src/components/Home/HomeScreenHeader.tsx
@@ -1,36 +1,64 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { info } from "./FriendsSection";
 import { formatNumber } from "../../utils/utilFunction";
 import CustomPressable from "../../UI/CustomPressable";
 import { useNavigation } from "@react-navigation/native";
 import { useColors } from "../../contexts/ColorContext";
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { FIREBASE_AUTH, FIREBASE_DB } from "../../firebase/FireBaseAuth";
+import { get, ref, set } from "firebase/database";
 
 const HomeScreenHeader = () => {
-  const {colors} = useColors()
-  const navigation = useNavigation<any>()
+  const { colors } = useColors();
+  const navigation = useNavigation<any>();
+  const [userName, setUserName] = useState<string | null>(null);
   let { balance } = info;
   let amountColor = balance >= 0 ? "#00bb00" : "red";
-  return (
-    <View style={styles.header}>
-      <View style={{ gap: 5 }}>
-        <Text style={{color: colors.primary_black, top: 10, fontSize: 26}}>{balance >= 0 ? "You are owed" : "You owe"}</Text>
-        <Text
-          style={[
-            styles.amount,
-            {
-              color: amountColor,
-            },
-          ]}
-        >
-          {`$${formatNumber(Math.abs(balance), 2)}`}
-        </Text>
-      </View>
 
-      <CustomPressable onPress={() => navigation.navigate("Notification")}>
-      <Ionicons name="notifications" size={31} color= "purple" />
-      </CustomPressable>
+  useEffect(() => {
+    const fetchData = async () => {
+      const user = FIREBASE_AUTH.currentUser;
+      if (user) {
+        const userRef = ref(FIREBASE_DB, `users/${user.uid}`);
+        const onSnapShot = await get(userRef);
+
+        if (onSnapShot.exists()) {
+          const userData = onSnapShot.val();
+          setUserName(userData.name);
+        }
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <View>
+      <Text style={{ color: "white", fontSize: 26, textAlign: "center", marginVertical: 10 }}>
+        Welcome {userName?.split(' ')[0]}
+      </Text>
+
+      <View style={styles.header}>
+        <View style={{ gap: 5 }}>
+          <Text style={{ color: colors.primary_black, top: 10, fontSize: 26 }}>
+            {balance >= 0 ? "You are owed" : "You owe"}
+          </Text>
+          <Text
+            style={[
+              styles.amount,
+              {
+                color: amountColor,
+              },
+            ]}
+          >
+            {`$${formatNumber(Math.abs(balance), 2)}`}
+          </Text>
+        </View>
+
+        <CustomPressable onPress={() => navigation.navigate("Notification")}>
+          <Ionicons name="notifications" size={31} color="purple" />
+        </CustomPressable>
+      </View>
     </View>
   );
 };
