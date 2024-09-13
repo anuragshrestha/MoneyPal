4f4f7ebe25c99571163a8ef049ffa1b6025f4c9d
Anurag Shrestha <anuragshrestha@Anurags-MacBook-Air.local>
2024-09-12 19:50:05 -0600

    fetch all user transation data from firestore

--- src/components/Home/FriendsSection.tsx
+++ src/components/Home/FriendsSection.tsx
@@ -1,10 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { useColors } from "../../contexts/ColorContext";
 import { formatNumber } from "../../utils/utilFunction";
+import { collection, doc, onSnapshot } from "firebase/firestore";
+import { FIREBASE_DB1, FIREBASE_AUTH } from "../../firebase/FireBaseAuth";
+
+const [transactions, setTransactions] = useState<Transaction[]>([]);
 
 export const info = {
-  name: "Anurag",
+  //name: "Anurag",
   last_month_return: 15.67,
   lifetime_return: 24.93,
   amount_lent: 2230,
@@ -12,43 +16,64 @@ export const info = {
   balance: 1345.01,
   friendList: [
     {
-      name: "Lax Adhikari",
+      name: "James Bond",
       amount_lent: 1450,
       interest_earned: 50.12,
       owes: 1500.12,
     },
     {
-      name: "Somiyo Rana",
-      amount_lent: 200,
-      interest_earned: 14.91,
-      owes: 214.91,
-    },
-    {
-      name: "Priya Dahal",
+      name: "Alex Rodriguez",
       amount_lent: -400,
-      interest_earned: -20.01,
-      owes: -420.01,
-    },
-    {
-      name: "Bikash",
-      amount_lent: -1600,
-      interest_earned: -50.12,
-      owes: -1670.12,
-    },
-    {
-      name: "Brian Fraze",
-      amount_lent: 580,
-      interest_earned: 30.12,
-      owes: 650.12,
+      interest_earned: -14.91,
+      owes: -414.91,
     },
   ],
 };
+
+interface Transaction {
+  name: string;
+  email: string;
+  amount: string;
+  transactionType: string;
+  interestEarned: string;
+}
+
+const fetchAllTransactions = useCallback(() => {
+  const user = FIREBASE_AUTH.currentUser;
+
+  if (user) {
+    const userDocRef = doc(FIREBASE_DB1, "transactions", user.uid);
+    const unsubscribe = onSnapshot(
+      collection(userDocRef, "userTransactions"),
+      (snapshot) => {
+        const transactionArray: Transaction[] = [];
+
+        snapshot.forEach((doc) => {
+          const data = doc.data() as Transaction;
+          transactionArray.push(data);
+        });
+        setTransactions(transactionArray);
+      },
+      (error) => {
+        console.log("Error fetchong real-time data: ", error);
+      }
+    );
+
+    return () => unsubscribe;
+  } else {
+    console.log("no user logged in");
+  }
+}, []);
 
 const FriendsSection = () => {
   const { colors } = useColors();
   return (
     <View style={{ marginTop: 25 }}>
-      <Text style={{color: colors.primary_black, fontSize: 20, marginBottom: 15 }}>Friends</Text>
+      <Text
+        style={{ color: colors.primary_black, fontSize: 20, marginBottom: 15 }}
+      >
+        Friends
+      </Text>
 
       {info.friendList.map((item, index) => {
         let positiveInterest = item.interest_earned >= 0;
@@ -57,16 +82,27 @@ const FriendsSection = () => {
         return (
           <View
             key={index}
-            style={[styles.conatiner, {borderBottomWidth: isLastItem ? 0 : 1}]}
+            style={[
+              styles.conatiner,
+              { borderBottomWidth: isLastItem ? 0 : 1 },
+            ]}
           >
             <View style={{ width: "37.5%" }}>
-              <Text style={{color: colors.primary_black,  letterSpacing: 1, fontSize: 18 }}>{item.name}</Text>
-              <Text style={{color: statusColor}} >${formatNumber(Math.abs(item.amount_lent), 2)}</Text>
+              <Text
+                style={{
+                  color: colors.primary_black,
+                  letterSpacing: 1,
+                  fontSize: 18,
+                }}
+              >
+                {item.name}
+              </Text>
+              <Text style={{ color: statusColor }}>
+                ${formatNumber(Math.abs(item.amount_lent), 2)}
+              </Text>
             </View>
 
-            <View
-              style={[styles.price, {backgroundColor: statusColor}]}
-            >
+            <View style={[styles.price, { backgroundColor: statusColor }]}>
               <Text style={{ color: "white" }}>
                 {positiveInterest ? "+" : "-"}$
                 {positiveInterest
@@ -89,7 +125,9 @@ const FriendsSection = () => {
                   ? "settled"
                   : "you owe"}
               </Text>
-              <Text style={{ color: statusColor, fontWeight: "bold" , fontSize: 16}}>
+              <Text
+                style={{ color: statusColor, fontWeight: "bold", fontSize: 16 }}
+              >
                 ${formatNumber(Math.abs(item.owes), 2)}
               </Text>
             </View>
@@ -103,7 +141,7 @@ const FriendsSection = () => {
 export default FriendsSection;
 
 const styles = StyleSheet.create({
-  conatiner:{
+  conatiner: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
@@ -117,5 +155,5 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     alignItems: "center",
     justifyContent: "center",
-  }
+  },
 });
