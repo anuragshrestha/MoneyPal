eb7d30877bb60bf0a2ae639f61b83965a27dd917
Anurag Shrestha <anuragshrestha16@gmail.com>
2024-09-14 18:19:05 -0600

    added friendsDetails screen inside the navigation stack

--- src/navigation/AccountStack.tsx
+++ src/navigation/AccountStack.tsx
@@ -3,8 +3,24 @@ import { View, Text } from "react-native";
 import "./../screens/AccountScreen";
 import AccountScreen from "./../screens/AccountScreen";
 import PaymentScreen from "../screens/PaymentScreen";
+import FriendsDetails from "../screens/FriendsDetails";
 
-const  Stack = createNativeStackNavigator();
+interface Transaction {
+    name: string;
+    email: string;
+    amount: string;
+    transactionType: string;
+    interest: string;
+    interestEarned: string;
+  }
+
+// Define the ParamList for your stack
+export type AccountStackParamList = {
+    Account: undefined;
+    SettlePayments: undefined;
+    FriendsDetails: { friend: Transaction };
+  };
+const  Stack = createNativeStackNavigator<AccountStackParamList>();
 
 const AccountStack = () => {
    
@@ -12,6 +28,7 @@ const AccountStack = () => {
         <Stack.Navigator screenOptions={{headerShown: false}}>
        <Stack.Screen name="Account" component={AccountScreen}/>
        <Stack.Screen name="SettlePayments" component={PaymentScreen} />
+       <Stack.Screen  name="FriendsDetails" component={FriendsDetails}/>
         </Stack.Navigator>
     )
 }
